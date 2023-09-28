31ac3b11cf574b24fd9a6cc2e0cfd2f6be22331a
“DmitriyU” <narian0611e@gmail.com>
2023-09-28 19:55:22 +0300

    Ваше сообщение о коммите

--- lvl_3/task_3.1.py
+++ lvl_3/task_3.1.py
@@ -54,57 +54,45 @@
 #   - использовать готовые классы numpy.array() и pandas.DataFrame() запрещено!
 #   - проявите фантазию :)
 
+from random import randint
+from itertools import product
+
+
 class Matrix:
-    def __init__(self, rows, columns):
-        self.rows = rows
-        self.columns = columns # Создаем пустую матрицу размером rows x columns
-        self.data = [[None for _ in range(columns)]
-                     for _ in range(rows)]
-    def get_rows(self):
-            return self.rows
-    def get_columns(self):
-            return self.columns
-    def set_value(self, row, column, value): # Проверяем, что индексы строки и столбца находятся внутри границ матрицы
-         if (0 <= row < self.rows and 0 <= column < self.columns):self.data[row][column] = value
-         else:
-             print("Ошибка: Неверные индексы строки или 1.")
-    def get_value(self, row, column): # Проверяем, что индексы строки и столбца находятся внутри границ матрицы
-     if (0 <= row < self.rows and 0 <= column < self.columns):
-         return self.data[row][column]
-     else:
-      print("Ошибка: Неверные индексы строки или столбца 2.")
-def print_size(self):
-    print("число строк", self.rows)
-    print("число столбцов", self.columns)
 
-matrix = Matrix(5, 4) # Создаем матрицу размером 5x4
-print("Строк:", matrix.get_rows()) # Вывод: 5
-print("Столбцов",matrix.get_columns()) # Вывод: 4
+    def __init__(self, line, column):
+        self.line = line
+        self.column = column
+        self.data = [[None for _ in range(column)] for _ in range(line)]
 
-matrix.set_value(0, 0, 10) # Устанавливаем значение 1 в ячейку (0, 0)
-matrix.set_value(0, 1, 20) # Устанавливаем значение 3 в ячейку (0, 1)
-matrix.set_value(0, 2, 30)
-matrix.set_value(0, 3, 40)
-matrix.set_value(1, 0, 11)
-matrix.set_value(1, 1, 12)
-matrix.set_value(1, 2, 13)
-matrix.set_value(1, 3, 14)
-matrix.set_value(2, 0, 21)
-matrix.set_value(2, 1, 22)
-matrix.set_value(2, 2, 23)
-matrix.set_value(2, 3, 24)
-matrix.set_value(3, 0, 31)
-matrix.set_value(3, 1, 32)
-matrix.set_value(3, 2, 33)
-matrix.set_value(3, 3, 34)
-matrix.set_value(4, 0, 41)
-matrix.set_value(4, 1, 42)
-matrix.set_value(4, 2, 43)
-matrix.set_value(4, 3, 44)
-print(matrix.get_value(0, 0), matrix.get_value(0, 1), matrix.get_value(0, 2), matrix.get_value(0, 3))
-print(matrix.get_value(1, 0), matrix.get_value(1, 1), matrix.get_value(1, 2), matrix.get_value(1, 3))
-print(matrix.get_value(2, 0), matrix.get_value(2, 1), matrix.get_value(2, 2), matrix.get_value(2, 3))
-print(matrix.get_value(3, 0), matrix.get_value(3, 1), matrix.get_value(3, 2), matrix.get_value(3, 3))
-print(matrix.get_value(4, 0), matrix.get_value(4, 1), matrix.get_value(4, 2), matrix.get_value(4, 3))
+    def len(self):
+        print('Количество строк ', len(self.data))
+        print('Количество столбцов ', len(self.data[0]))
+
+    def __str__(self):
+        return "\n".join(map(str, self.data))
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self.line!r}, {self.column!r})'
+
+    def add(self, add_list):
+        values = iter(add_list)
+
+        indices = product(range(self.line), range(self.column))
+
+        for (i, j), value in zip(indices, values):
+            self.data[i][j] = value
+
+        return values
+
+    def replace(self, line_index, column_index, value):
+        self.data[line_index][column_index] = value
 
 
+my_matrix = Matrix(10, 10)
+overflow = my_matrix.add([randint(1, 9) for i in range(100)])
+
+print(my_matrix)
+my_matrix.len()
+my_matrix.replace(7, 8, 9)
+print(my_matrix)
